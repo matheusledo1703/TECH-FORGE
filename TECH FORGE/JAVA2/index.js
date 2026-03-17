510deeaec77da900d38dccda2ecdb160979d2154
Matheus Ledo <matheus.ledo@grupointegrado.br>
2026-03-17 20:24:41 -0300

    Criação do Git e inserção dos arquivos JS

---
+++ TECH FORGE/JAVA2/index.js
@@ -0,0 +1,16 @@
+const prompt = require ("prompt-sync")();
+
+let nome = prompt("Digite seu nome.");
+console.log ("O nome digitado foi:" + nome);
+
+let cor = prompt("Digite sua cor favorita.");
+console.log ("A cor favorita digitada foi:" + cor);
+
+let filme = prompt("Digite seu filme preferido.");
+console.log ("O filme preferido digita foi:" + filme);
+
+let cidade = prompt("Digite a cidade que você mora.");
+console.log ("A cidade digitada foi:" + cidade);
+
+let animal = prompt("Digite seu animal preferido.");
+console.log ("O animal preferido digita foi:" + animal);
